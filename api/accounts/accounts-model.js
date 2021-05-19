cd1bd24b5830c577ba6dded15a36a064aed6bda3
matt22881 <matt22881@gmail.com>
2021-05-19 04:29:34 -0400

    Add files via upload

---
+++ api/accounts/accounts-model.js
@@ -0,0 +1,27 @@
+const getAll = () => {
+  // DO YOUR MAGIC
+}
+
+const getById = id => {
+  // DO YOUR MAGIC
+}
+
+const create = account => {
+  // DO YOUR MAGIC
+}
+
+const updateById = (id, account) => {
+  // DO YOUR MAGIC
+}
+
+const deleteById = id => {
+  // DO YOUR MAGIC
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  updateById,
+  deleteById,
+}
---
+++ api/accounts/accounts-router.js
@@ -0,0 +1,27 @@
+const router = require('express').Router()
+
+router.get('/', (req, res, next) => {
+  // DO YOUR MAGIC
+})
+
+router.get('/:id', (req, res, next) => {
+  // DO YOUR MAGIC
+})
+
+router.post('/', (req, res, next) => {
+  // DO YOUR MAGIC
+})
+
+router.put('/:id', (req, res, next) => {
+  // DO YOUR MAGIC
+});
+
+router.delete('/:id', (req, res, next) => {
+  // DO YOUR MAGIC
+})
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  // DO YOUR MAGIC
+})
+
+module.exports = router;
